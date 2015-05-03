40ae6501472e4693b6cf8ecab650ab6bfb414cdb
TCMOREIRA <tomazcorralmoreira@gmail.com>
2015-05-03 02:01:44 -0300

    First Commit
    Feel Good Inc.

---
+++ RevolutionDancers/desktop/src/com/RevolutionDancers/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.RevolutionDancers.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.RevolutionDancers.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
